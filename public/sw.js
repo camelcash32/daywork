a22b0d4d44c6a830a897da12b1ebb612c8b84356
camelcash32 <lcount321@gmail.com>
2026-03-30 08:33:02 -0700

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,6 @@
-// DAYWORK Service Worker
-const CACHE = 'daywork-v1';
-const ASSETS = ['/', '/index.html', '/landing'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {})
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
+const CACHE = 'daywork-v2';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { self.clients.claim(); });
 self.addEventListener('fetch', e => {
-  // Network first, fallback to cache
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
